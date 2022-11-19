321e4bf1d9d28c5972658f1fe1884a509f868c6c
Peikrishvili <ypeikris@its.jnj.com>
2022-11-19 14:51:11 +0300

    first commit

---
+++ src/components/countersList.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+    const initialState = [
+        { id: 0, value: 0, name: "Ненужная вещь", price: "200" },
+        { id: 1, value: 4, name: "Ложка" },
+        { id: 2, value: 0, name: "Вилка" },
+        { id: 3, value: 0, name: "Тарелка" },
+        { id: 4, value: 0, name: "Набор минималиста" },
+    ];
+
+    const [counters, setCounters] = useState(initialState);
+    const handleDelete = (id) => {
+        const newCounters = counters.filter((c) => c.id !== id);
+        setCounters(newCounters);
+    };
+    const handleReset = () => {
+        setCounters(initialState);
+        console.log("handle reset");
+    };
+
+    return (
+        <>
+            {counters.map((count) => (
+                <Counter key={count.id} onDelete={handleDelete} {...count} />
+            ))}
+            <button
+                className='btn btn-primary btn-sm m-2'
+                onClick={handleReset}
+            >
+                Сброс
+            </button>
+        </>
+    );
+};
+export default CountersList;
